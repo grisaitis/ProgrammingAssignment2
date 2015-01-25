10779fbd5bdf6d2ff5eb814586b19d0e95dd3f4c
William Grisaitis <wgrisaitis@gmail.com>
2015-01-25 17:18:09 -0500

    improving comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,16 @@
 ## These functions provide a framework for managing the computation of inverting
-# a matrix.
+# a matrix. 
 
-## this function returns a list of functions for managing the  inversion of a
-# matrices. It's sort of like defining a class (with attributes and methods),
-# but R doesn't have class definitions, and this is the next best thing.
 
-## The specific methods this list has are:
+## The makeCacheMatrix() function returns a list of functions for managing the
+# inversion of a matrix. It's sort of like defining a class (with attributes
+# and methods), but R doesn't have class definitions, and this is the next best
+# thing.
+# The specific methods this list has are:
 # * get() - returns the matrix you want to invert
 # * set(y) - changes the matrix you want to invert, to y (a matrix object)
 # * getSolution() - returns the inverse of the matrix, if stored.
 # * setSolution(newSolution) - changes the stored inverse of the matrix
-
 
 makeCacheMatrix <- function(x = matrix()) {
     solution = NULL
