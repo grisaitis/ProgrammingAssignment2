47b1ffdaf34de600fb48a3bc06f76ef6aea5674a
William Grisaitis <wgrisaitis@gmail.com>
2015-01-25 16:38:35 -0500

    initial draft of makeCacheMatrix()

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    solution = NULL
+    set = function(y) {
+        # syntax note: double arrows change the variable in the parent namespace
+        x <<- y # assign a new matrix to X
+        solution <<- NULL # reset the solution, because x has changed
+    }
+    get = function() {
+        x # return the matrix X
+    }
+    setSolution <- function(newSolution) {
+        solution <<- newSolution # store a new inverse of X
+    }
+    getSolution <- function() {
+        solution # return the inverse of X, as currently stored
+    }
+    list(set = set
+        ,get = get
+        ,setSolution = setSolution
+        ,getSolution = getSolution
+        )
 }
 
 
